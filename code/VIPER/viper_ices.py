eeb31d0e985a3edd0f6bbea22b08f4db10de55db
yejinyeo <yeding1720@gmail.com>
2025-02-06 05:30:53 +0000

    변형된 word.txt 파일과 변형전후 연결 word.txt 파일 생성 기능 추가

--- code/VIPER/viper_ices.py
+++ code/VIPER/viper_ices.py
@@ -14,9 +14,10 @@ parser.add_argument('-e', action="store", dest="embed")
 parser.add_argument("-p", action="store", dest="prob")
 parser.add_argument('-s', action="store", dest="seed", type=int, default=42)
 parser.add_argument('--perturbations-file', action="store", dest='perturbations_file')
+parser.add_argument('--transformed-file', action="store", dest='transformed_file', default="transformed_words.txt")
+parser.add_argument('--linked-file', action="store", dest='linked_file', default="linked_words.txt")
 
 parsed_args = parser.parse_args(sys.argv[1:])
-
 seed = parsed_args.seed
 random.seed(seed)
 np.random.seed(seed)
@@ -25,8 +26,14 @@ emb = parsed_args.embed
 prob = float(parsed_args.prob)
 perturbations_file = PerturbationsStorage(parsed_args.perturbations_file)
 
+transformed_lines_file = parsed_args.transformed_file
+linked_lines_file = parsed_args.linked_file
+
+open(transformed_lines_file, "w", encoding="utf-8").close()
+open(linked_lines_file, "w", encoding="utf-8").close()
+
 # SAMPLE USAGE:
-# python3 viper_ices.py -e ../embeddings/efile.norm -p 0.4 -s 123 --perturbations-file dummy_store.txt < unsafe_raw_words.txt
+# python3 viper_ices.py -e ../embeddings/efile.norm -p 0.4 -s 123 --perturbations-file dummy_store.txt --transformed-file output_transformed_lines.txt --linked-file output_linked_lines.txt < unsafe_raw_words.txt
 
 model = W2Vec.load_word2vec_format(emb)
 
@@ -37,33 +44,49 @@ topn = 20
 mydict = {}
 
 for line in sys.stdin:
-    a = line.split()
-    wwords = []
-    out_x = []
-    for w in a:
-        for c in w:
-            if c not in mydict:
-                similar = model.most_similar(c, topn=topn)
+    original_line = line.strip()  # 원본 라인
+    transformed_line = []  # 변형된 라인
+
+    # 단어 단위 처리
+    for word in original_line.split():
+        transformed_word = []  # 변형된 단어
+
+        # 문자 단위 처리
+        for char in word:
+            if char not in mydict:
+                # 유사한 문자 계산
+                similar = model.most_similar(char, topn=topn)
                 if isOdd:
                     similar = [similar[iz] for iz in range(1, len(similar), 2)]
                 elif isEven:
                     similar = [similar[iz] for iz in range(0, len(similar), 2)]
                 words, probs = [x[0] for x in similar], np.array([x[1] for x in similar])
                 probs /= np.sum(probs)
-                mydict[c] = (words, probs)
+                mydict[char] = (words, probs)
             else:
-                words, probs = mydict[c]
-            r = random.random()
-            if r < prob:
-                s = np.random.choice(words, 1, replace=True, p=probs)[0]
-                perturbations_file.add(c, s)
+                words, probs = mydict[char]
+
+            # 확률에 따라 변형 적용
+            if random.random() < prob:
+                transformed_char = np.random.choice(words, 1, replace=True, p=probs)[0]
+                perturbations_file.add(char, transformed_char)
             else:
-                s = c
-            out_x.append(s)
-        # out_x.append(" ")
-        wwords.append("".join(out_x))
-        out_x = []
+                transformed_char = char
+            transformed_word.append(transformed_char)
 
-    print(" ".join(wwords))
+        # 변형된 단어 추가
+        transformed_line.append("".join(transformed_word))
 
+    # 변형된 라인 생성
+    transformed_line_str = " ".join(transformed_line)
+
+    # 변형된 텍스트 저장
+    with open(transformed_lines_file, "a", encoding="utf-8") as transformed_file:
+        transformed_file.write(transformed_line_str + "\n")
+
+    # 변형 전후 텍스트 연결 저장
+    with open(linked_lines_file, "a", encoding="utf-8") as linked_file:
+        linked_file.write(f"{original_line}||{transformed_line_str}\n")
+
+# 변형 로그 저장
 perturbations_file.maybe_write()
